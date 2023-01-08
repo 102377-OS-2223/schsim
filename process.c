110b3c3246a451a1cf39ef629efd79d73c08a4d2
Fullettortuga <carlosmg.12@hotmail.es>
2023-01-08 01:06:35 +0100

    Funció ToString.

--- process.c
+++ process.c
@@ -19,22 +19,22 @@ void cleanProcess(Process proces){
 
 Process initProcessFromTokens(char* line, char* separator){
 
-  const size_t BUFSIZE = 32;   
-  size_t bufsize = BUFSIZE;
-  Process p;
+  const size_t BUFSIZE = 32; // Quantitat de memoria per al Buffer
+  size_t bufsize = BUFSIZE; 
+  Process p; // Procès
 
-  char *feature = malloc(bufsize * sizeof(char));
-  feature=strtok(line, separator); 
+  char *feature = malloc(bufsize * sizeof(char)); // Reserva de memoria.
+  feature=strtok(line, separator); // Separar String por tokens.
 
   int features = 0;
   while (feature != NULL) {
             switch (features)
             {
             case 0:
-                p.id=atoi(feature);
+                p.id=atoi(feature); // Retorna INT de id.
                 break;
             case 1:
-                p.name=malloc(sizeof(feature));
+                p.name=malloc(sizeof(feature)); // Resrva memoria per al nom del procès
                 strcpy(p.name,feature);
                 break;
             case 2:
@@ -49,8 +49,8 @@ Process initProcessFromTokens(char* line, char* separator){
             feature=strtok(NULL, ";"); 
             features++;
     }
-  free(feature);
-  return p;
+  free(feature); // Llibera memoria
+  return p; // retorna procès
 }
 
 int compareArrival(const void *s1, const void *s2){
@@ -89,8 +89,21 @@ int comparePriority(const void *s1, const void *s2){
         return 0;
 }
 
+/*int Compareall(const void *s1, const void *s2,){
+    Process *p1 = (Process *)s1;
+    Process *p2 = (Process *)s2;
+    if (p1-> > p2) 
+        return 1; 
+    else if (p1 < p2)  
+        return -1;
+    else
+        return 0;
+}*/
+
 //@TODO: Aquesta funció ha de retorna un cadena de text amb la següent info:
 //{id:0; name:A; burst:7; priority:9; arrive_time:0; lifecycle:[EEEPPEF]; ...} 
-/* char* procToString(Process* p){
+
+ /*char* procToString(Process* p){
+
 
 }*/
